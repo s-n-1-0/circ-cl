301a535fc22803df4627ec6d1afaf519305e66f9
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-08-06 13:54:40 +0900

    百人一首カード全てを表示するcardsページの追加

--- card_flask/app/__init__.py
+++ card_flask/app/__init__.py
@@ -3,12 +3,17 @@ from .xls.read_xls import get_card
 
 app = Flask(__name__)
 @app.route("/")
-def hello():
+def top_page():
     return "Top Page"
 
 @app.route("/card/<num>")
-def index(num):
+def card(num):
     card_num = int(num)
     card_num = abs(card_num)
     card = get_card(card_num)
-    return render_template("index.html",card=card)
+    return render_template("card.html",card=card)
+
+@app.route("/cards")
+def cards():
+    cards = [get_card(i+1) for i in range(100)]
+    return render_template("cards.html",cards=cards)
